952f8cb9e3a160e66f4d21e84e3d28d1e43acf8e
Carlos Roberto <carlosmedeiroslima@gmail.com>
2022-01-22 15:04:26 -0300

    Atualização JavaFX

--- src/br/com/roberto/fx/layout/AppLayout.java
+++ src/br/com/roberto/fx/layout/AppLayout.java
@@ -1,8 +1,8 @@
 package br.com.roberto.fx.layout;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application{
@@ -10,6 +10,19 @@ public class AppLayout extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		Parent raiz = null;
+		//raiz = new TesteAnchorPane();
+		//raiz = new TesteBorderPane();
+		//raiz = new TesteFlowPane();
+		//raiz = new TesteGridPane();
+		//raiz = new TesteStackPane();
+		raiz = new TesteTilePane();
+		
+		
+		//Scene principal = new Scene(new TesteAnchorPane(), 800, 600);
+		Scene principal = new Scene(raiz, 800, 600);
+		
+		primaryStage.setScene(principal);
 		primaryStage.setTitle("Gerenciadores de Layout");
 		primaryStage.show();
 		
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,10 @@
 module exerciciosfx {
 
 	requires javafx.controls;
+	requires javafx.fxml;
+	requires org.controlsfx.controls;
+	
 	opens br.com.roberto.fx.basico;
 	opens br.com.roberto.fx.layout;
+	opens br.com.roberto.fx.fxml;
 }
